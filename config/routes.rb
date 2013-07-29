a8e5c9ca69bb5847a72bf62db2462c7320320194
Diego Marcet <diego.marcet@tipit.net>
2013-07-29 14:08:54 -0300

    Fixed routes to include resource's ID

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Tweek::Application.routes.draw do
-  resource :users, :only => [:create, :update, :destroy]
+  resources :users, :only => [:create, :update, :destroy]
 
-  resource :movies, :only => [:create, :update, :destroy]
+  resources :movies, :only => [:create, :update, :destroy]
 
   resource :likes, :only => [:create, :destroy] 
 end
