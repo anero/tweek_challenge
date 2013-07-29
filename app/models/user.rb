9c39ab6629a077bca2840316579f648143d8099f
Diego Marcet <diego.marcet@tipit.net>
2013-07-29 11:29:39 -0300

    Added validation for email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name
 
   validates_uniqueness_of :email
+  validates_format_of :email, :with => /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+$/
 end
