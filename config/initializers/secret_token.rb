855c4c814b5be5f1614c5ee91038e9a4e8128ab8
Diego Marcet <diego.marcet@tipit.net>
2013-07-29 09:28:05 -0300

    Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweek::Application.config.secret_token = 'cd56f56d4f59c894cf51f3b5063905f32f4cc679e2276020b915283a2a68f35953c99eeb074617558c014eb0c1e938e97ec26c2deacd6aa983b3ed4bd50de893'
